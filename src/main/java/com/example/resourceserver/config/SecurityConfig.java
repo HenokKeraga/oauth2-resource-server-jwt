c498c0aada29028a853ce58e230538c0e18413ac
henok keraga <henokkeraga@gmail.com>
2024-01-12 17:00:21 -0700

    resource server - jwt token

--- src/main/java/com/example/resourceserver/config/SecurityConfig.java
+++ src/main/java/com/example/resourceserver/config/SecurityConfig.java
@@ -3,7 +3,11 @@ package com.example.resourceserver.config;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.security.authentication.AuthenticationManagerResolver;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
+import org.springframework.security.oauth2.server.resource.authentication.JwtIssuerAuthenticationManagerResolver;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.cors.CorsConfiguration;
@@ -14,12 +18,15 @@ import java.util.List;
 @Configuration
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity,
+                                                   AuthenticationManagerResolver<HttpServletRequest> authenticationManagerResolver) throws Exception {
 
         httpSecurity
                 .authorizeHttpRequests(a -> a.anyRequest().authenticated());
-        httpSecurity
-                .oauth2ResourceServer(configurer -> configurer.jwt(jwtConfigurer -> jwtConfigurer.jwkSetUri("http://localhost:8080/oauth2/jwks")));
+//        httpSecurity
+//                .oauth2ResourceServer(configurer -> configurer.jwt(jwtConfigurer -> jwtConfigurer.jwkSetUri("http://localhost:8080/oauth2/jwks")));
+
+        httpSecurity.oauth2ResourceServer(configurer -> configurer.authenticationManagerResolver(authenticationManagerResolver));
         httpSecurity
                 .cors(httpSecurityCorsConfigurer -> {
                     var c = new CorsConfigurationSource() {
@@ -37,5 +44,14 @@ public class SecurityConfig {
                 });
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public AuthenticationManagerResolver<HttpServletRequest> authenticationManagerResolver() {
+
+        JwtIssuerAuthenticationManagerResolver jwtIssuerAuthenticationManagerResolver =
+                JwtIssuerAuthenticationManagerResolver.fromTrustedIssuers("http://localhost:9999");
+
+        return jwtIssuerAuthenticationManagerResolver;
     }
 }
